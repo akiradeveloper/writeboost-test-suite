b0d6ad0326307e7aebb8c8aef85529e0211c65ca
Akira Hayakawa <ruby.wktk@gmail.com>
2016-06-24 11:40:53 +0900

    pipe and redirection is not allowed in Shell
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/main/scala/dmtest/DMStack.scala
+++ src/main/scala/dmtest/DMStack.scala
@@ -6,7 +6,6 @@ trait DMStack extends Stack {
   def dm: DMState
   override def path = Paths.get(s"/dev/mapper/${dm.name}")
   def reload[S <: DMStackDecorator[S]](table: DMTable[S]): S = {
-    dm.suspend(); dm.resume() // debug
     dm.suspend()
     val res = table.f(this) // does reload on creating the new stack
     dm.resume()
--- src/main/scala/dmtest/DMState.scala
+++ src/main/scala/dmtest/DMState.scala
@@ -1,9 +1,20 @@
 package dmtest
 
+import java.io.{FileWriter, File}
+
 class DMState(val name: String) {
   def create() = Shell(s"dmsetup create ${name} --notable")
   def remove() = Shell(s"dmsetup remove ${name}")
-  def reload(table: String) = Shell(s"echo ${table} | dmsetup reload ${name}")
+  def reload(table: String) = {
+    val fn = s"/tmp/${RandNameAllocator.alloc}"
+    val f = new File(fn); // f.deleteOnExit()
+    val writer = new FileWriter(fn)
+    writer.write(table)
+    writer.close()
+
+    Shell(s"dmsetup reload ${name} ${fn}")
+    Shell(s"rm ${fn}")
+  }
   def suspend() = Shell(s"dmsetup suspend ${name}")
   def resume() = Shell(s"dmsetup resume ${name}")
   def table() = Shell(s"dmsetup table ${name}")
