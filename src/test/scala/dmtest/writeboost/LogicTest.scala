d7b0a913b944d3a4d89891f8eed4ad21ba2c4003
Akira Hayakawa <ruby.wktk@gmail.com>
2016-06-30 15:07:06 +0900

    use PatternedSeqIOe
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/test/scala/dmtest/writeboost/LogicTest.scala
+++ src/test/scala/dmtest/writeboost/LogicTest.scala
@@ -32,7 +32,11 @@ class LogicTest extends DMTestSuite {
       fastDevice(cachingSize) { caching =>
         Writeboost.sweepCaches(caching)
         Writeboost.Table(backing, caching, Map("writeback_threshold" -> 0, "nr_max_batched_writeback" -> 1)).create { s =>
-          Shell(s"fio --name=test --time_based --runtime=${30 <> 3} --filename=${s.bdev.path} --rw=write:3584 --ioengine=libaio --direct=1 --bs=512")
+          // Shell(s"fio --name=test --time_based --runtime=${30 <> 3} --filename=${s.bdev.path} --rw=write:3584 --ioengine=libaio --direct=1 --bs=512")
+          val pat = Seq(PatternedSeqIO.Write(Sector(1)), PatternedSeqIO.Skip(Sector(7)))
+          val pio = new PatternedSeqIO(pat)
+          pio.maxRuntime = (30 <> 3) * 1000 // ms
+          pio.run(s)
         }
       }
     }
@@ -42,10 +46,14 @@ class LogicTest extends DMTestSuite {
       fastDevice(Sector.M(32)) { caching =>
         Writeboost.sweepCaches(caching)
         Writeboost.Table(backing, caching).create { s =>
-          def run() = Shell(s"fio --name=test filename=${s.bdev.path} --io_limit=16M --rw=read:4K --bs=4K --direct=1")
-          run()
+          import PatternedSeqIO._
+          val pat = Seq(Read(Sector.K(4)), Skip(Sector.K(4)))
+          val pio = new PatternedSeqIO(pat)
+          pio.maxIOAmount = Sector.M(16)
+          // def run() = Shell(s"fio --name=test filename=${s.bdev.path} --io_limit=16M --rw=read:4K --bs=4K --direct=1")
+          pio.run(s)
           val st1 = Writeboost.Status.parse(s.dm.status())
-          run()
+          pio.run(s)
           val st2 = Writeboost.Status.parse(s.dm.status())
           val key = Writeboost.StatKey(false, true, false, true)
           assert(st2.stat(key) === st1.stat(key))
@@ -58,13 +66,19 @@ class LogicTest extends DMTestSuite {
       fastDevice(Sector.M(32)) { caching =>
         val table = Writeboost.Table(backing, caching, Map("read_cache_threshold" -> 1))
         Writeboost.sweepCaches(caching)
-        def run(s: Stack) = Shell(s"fio --name=test --filename=${s.bdev.path} --io_limit=16M --rw=read:4K --bs=4K --direct=1")
+        // def run(s: Stack) = Shell(s"fio --name=test --filename=${s.bdev.path} --io_limit=16M --rw=read:4K --bs=4K --direct=1")
+        import PatternedSeqIO._
+        val pat = Seq(Read(Sector.K(4)), Skip(Sector.K(4)))
+        val pio = new PatternedSeqIO(pat)
+        pio.maxIOAmount = Sector.M(16)
         val st1 = table.create { s =>
-          run(s)
+          // run(s)
+          pio.run(s)
           Writeboost.Status.parse(s.dm.status())
         }
         val st2 = table.create { s =>
-          run(s)
+          // run(s)
+          pio.run(s)
           Writeboost.Status.parse(s.dm.status())
         }
         val key = Writeboost.StatKey(false, true, false, true)
