a85ae8abe486fcd32986f49ad1ba6973faf961af
Akira Hayakawa <ruby.wktk@gmail.com>
2016-06-30 15:18:12 +0900

    writeoboost: add stacking test
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/test/scala/dmtest/writeboost/StackTest.scala
+++ src/test/scala/dmtest/writeboost/StackTest.scala
@@ -3,5 +3,17 @@ package dmtest.writeboost
 import dmtest.DMTestSuite
 import dmtest.stack.Writeboost
 
+import dmtest._
+import dmtest.stack._
+
 class StackTest extends DMTestSuite {
+  test("just stack") {
+    slowDevice(Sector.M(16)) { backing =>
+      fastDevice(Sector.M(4)) { caching =>
+        Writeboost.Table(backing, caching).create { s =>
+          assert(s.exists)
+        }
+      }
+    }
+  }
 }
