5d205e9657bf3a911d1f353e50b199ff6e9b4fb9
Akira Hayakawa <ruby.wktk@gmail.com>
2016-07-02 19:57:41 +0900

    wip
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/test/scala/dmtest/writeboost/WIP.scala
+++ src/test/scala/dmtest/writeboost/WIP.scala
@@ -6,4 +6,23 @@ import dmtest.fs._
 import scala.util.Random
 
 class WIP extends DMTestSuite {
+  test("partial IO works") {
+    slowDevice(Sector.M(128)) { backing =>
+      fastDevice(Sector.M(4)) { caching =>
+        Writeboost.sweepCaches(caching)
+        Writeboost.Table(backing, caching).create { s =>
+          val st1 = s.status
+          s.bdev.write(Sector(0), DataBuffer.random(Sector(1).toB.toInt))
+          val st2 = s.status
+          val key1 = Writeboost.StatKey(true, false, false, false)
+          assert(st2.stat(key1) > st1.stat(key1))
+
+          s.bdev.read(Sector(8), Sector(1))
+          val st3 = s.status
+          val key2 = Writeboost.StatKey(false, false, false, false)
+          assert(st3.stat(key2) > st2.stat(key2))
+        }
+      }
+    }
+  }
 }
