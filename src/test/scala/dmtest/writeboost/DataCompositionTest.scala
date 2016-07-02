18bea8763f71342a723f54685b908474e3c45013
Akira Hayakawa <ruby.wktk@gmail.com>
2016-07-02 16:45:14 +0900

    add random composition test
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

---
+++ src/test/scala/dmtest/writeboost/DataCompositionTest.scala
@@ -0,0 +1,212 @@
+package dmtest.writeboost
+
+import dmtest.stack.Writeboost
+import dmtest.{DataBuffer, Sector, DMTestSuite}
+import scala.util.Random
+
+class DataCompositionTest extends DMTestSuite {
+  test("read: no writeboost") {
+    slowDevice(Sector.G(1)) { backing =>
+      val base = DataBuffer.random(Sector(8).toB.toInt)
+      backing.bdev.write(Sector(0), base)
+      assert(backing.bdev.read(Sector(0), Sector(8)) isSameAs base)
+    }
+  }
+  test("read: no caching") {
+    slowDevice(Sector.G(1)) { backing =>
+      fastDevice(Sector.M(32)) { caching =>
+        val base = DataBuffer.random(Sector(8).toB.toInt)
+        backing.bdev.write(Sector(0), base)
+
+        Writeboost.sweepCaches(caching)
+        Writeboost.Table(backing, caching).create { s =>
+          val st1 = s.status
+          assert(s.bdev.read(Sector(0), Sector(8)) isSameAs base)
+          val st2 = s.status
+          val key = Writeboost.StatKey(false, false, false, true)
+          assert(st2.stat(key) > st1.stat(key))
+        }
+      }
+    }
+  }
+  test("read: rambuf data + backing") {
+    slowDevice(Sector.G(1)) { backing =>
+      fastDevice(Sector.M(32)) { caching =>
+        val base = DataBuffer.random(Sector(8).toB.toInt)
+        backing.bdev.write(Sector(0), base)
+
+        Writeboost.sweepCaches(caching)
+        Writeboost.Table(backing, caching).create { s =>
+          val D1 = DataBuffer.random(Sector(1).toB.toInt)
+          s.bdev.write(Sector(1), D1)
+
+          // bb 11 bb bb bb bb bb bb
+          val shouldRead: DataBuffer = base.overwrite(Sector(1).toB.toInt, D1)
+          val st1 = s.status
+          assert(s.bdev.read(Sector(0), Sector(8)) isSameAs shouldRead)
+          val st2 = s.status
+          val key = Writeboost.StatKey(false, true, true, true)
+          assert(st2.stat(key) > st1.stat(key))
+        }
+      }
+    }
+  }
+  test("read: cached data + backing") {
+    slowDevice(Sector.G(1)) { backing =>
+      fastDevice(Sector.M(32)) { caching =>
+        val base = DataBuffer.random(Sector(8).toB.toInt)
+        backing.bdev.write(Sector(0), base)
+
+        Writeboost.sweepCaches(caching)
+        Writeboost.Table(backing, caching).create { s =>
+          val D1 = DataBuffer.random(Sector(1).toB.toInt)
+          s.bdev.write(Sector(1), D1) // rambuf
+
+          s.dropTransient()
+
+          // bb 11 bb bb bb bb bb bb
+          val shouldRead: DataBuffer = base.overwrite(Sector(1).toB.toInt, D1)
+          val st1 = s.status
+          assert(s.bdev.read(Sector(0), Sector(8)) isSameAs shouldRead)
+          val st2 = s.status
+          val key = Writeboost.StatKey(false, true, false, true)
+          assert(st2.stat(key) > st1.stat(key))
+        }
+      }
+    }
+  }
+  test("read: rambuf fully overwrite cached data + backing"){
+    slowDevice(Sector.G(1)) { backing =>
+      fastDevice(Sector.M(32)) { caching =>
+        val base = DataBuffer.random(Sector(8).toB.toInt)
+        backing.bdev.write(Sector(0), base)
+
+        Writeboost.sweepCaches(caching)
+        Writeboost.Table(backing, caching).create { s =>
+          val D1 = DataBuffer.random(Sector(1).toB.toInt)
+          s.bdev.write(offset = Sector(1), D1)
+
+          s.dropTransient()
+
+          val D2 = DataBuffer.random(Sector(2).toB.toInt)
+          s.bdev.write(offset = Sector(0), D2)
+
+          // 22 22 bb bb bb bb bb bb
+          val shouldRead: DataBuffer = base
+            .overwrite(Sector(1).toB.toInt, D1)
+            .overwrite(Sector(0).toB.toInt, D2)
+          val st1 = s.status
+          assert(s.bdev.read(Sector(0), Sector(8)) isSameAs shouldRead)
+          val st2 = s.status
+          val key = Writeboost.StatKey(false, true, true, true)
+          assert(st2.stat(key) > st1.stat(key))
+        }
+      }
+    }
+  }
+  test("read: rambuf partially overwrites cached data + backing") {
+    slowDevice(Sector.G(1)) { backing =>
+      fastDevice(Sector.M(32)) { caching =>
+        val base = DataBuffer.random(Sector(8).toB.toInt)
+        backing.bdev.write(Sector(0), base)
+
+        Writeboost.sweepCaches(caching)
+        Writeboost.Table(backing, caching).create { s =>
+          val D1 = DataBuffer.random(Sector(2).toB.toInt)
+          s.bdev.write(offset = Sector(1), D1)
+
+          s.dropTransient()
+
+          val D2 = DataBuffer.random(Sector(1).toB.toInt)
+          s.bdev.write(offset = Sector(1), D2)
+
+          // bb 22 11 bb bb bb bb bb
+          val shouldRead: DataBuffer = base
+            .overwrite(Sector(1).toB.toInt, D1)
+            .overwrite(Sector(1).toB.toInt, D2)
+          val st1 = s.status
+          assert(s.bdev.read(Sector(0), Sector(8)) isSameAs shouldRead)
+          val st2 = s.status
+          val key = Writeboost.StatKey(false, true, true, true)
+          assert(st2.stat(key) > st1.stat(key))
+        }
+      }
+    }
+  }
+  test("read: rambuf not overwrite cached data + backing") {
+    slowDevice(Sector.G(1)) { backing =>
+      fastDevice(Sector.M(32)) { caching =>
+        val base = DataBuffer.random(Sector(8).toB.toInt)
+        backing.bdev.write(Sector(0), base)
+
+        Writeboost.sweepCaches(caching)
+        Writeboost.Table(backing, caching).create { s =>
+          val D1 = DataBuffer.random(Sector(1).toB.toInt)
+          s.bdev.write(offset = Sector(1), D1)
+
+          s.dropTransient()
+
+          val D2 = DataBuffer.random(Sector(2).toB.toInt)
+          s.bdev.write(offset = Sector(2), D2)
+
+          // bb 11 22 22 bb bb bb bb
+          val shouldRead: DataBuffer = base
+            .overwrite(Sector(1).toB.toInt, D1)
+            .overwrite(Sector(2).toB.toInt, D2)
+          val st1 = s.status
+          assert(s.bdev.read(Sector(0), Sector(8)) isSameAs shouldRead)
+          val st2 = s.status
+          val key = Writeboost.StatKey(false, true, true, true)
+          assert(st2.stat(key) > st1.stat(key))
+        }
+      }
+    }
+  }
+  test("read: random write and occasionally dropping rambuf") {
+    trait Input
+    case class Write(offset: Sector, data: DataBuffer) extends Input
+    case class DropTransient() extends Input
+    def compose(acc: DataBuffer, x: Input): DataBuffer = {
+      x match {
+        case Write(offset: Sector, data: DataBuffer) =>
+          acc.overwrite(offset.toB.toInt, data)
+        case DropTransient() =>
+          acc
+      }
+    }
+    val base = DataBuffer.random(Sector(8).toB.toInt)
+    val inputs = (0 until 10000).map { _ =>
+      Random.nextInt(3) match {
+        case 3 =>
+          DropTransient()
+        case _ =>
+          val offset = Random.nextInt(8)
+          val len = Random.nextInt(8 - offset)
+          val data = DataBuffer.random(Sector(len).toB.toInt)
+          Write(Sector(offset), data)
+      }
+    }
+    slowDevice(Sector.G(1)) { backing =>
+      fastDevice(Sector.M(32)) { caching =>
+        backing.bdev.write(Sector(0), base)
+
+        Writeboost.sweepCaches(caching)
+        Writeboost.Table(backing, caching).create { s =>
+          var acc = base
+          inputs.foreach { input =>
+            input match {
+              case Write(offset, data) =>
+                s.bdev.write(offset, data)
+              case DropTransient() =>
+                s.dropTransient()
+            }
+            val read = s.bdev.read(Sector(0), Sector(8))
+
+            acc = compose(acc, input)
+            assert(read isSameAs acc)
+          }
+        }
+      }
+    }
+  }
+}
