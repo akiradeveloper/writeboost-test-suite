522855e5b4489b0cbe251e7ab6a3af6725f09856
Akira Hayakawa <ruby.wktk@gmail.com>
2016-07-03 18:39:18 +0900

    wip
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/test/scala/dmtest/writeboost/DataCompositionTest.scala
+++ src/test/scala/dmtest/writeboost/DataCompositionTest.scala
@@ -12,6 +12,25 @@ class DataCompositionTest extends DMTestSuite {
       assert(backing.bdev.read(Sector(0), Sector(8)) isSameAs base)
     }
   }
+  test("partial IO works") {
+    slowDevice(Sector.M(128)) { backing =>
+      fastDevice(Sector.M(4)) { caching =>
+        Writeboost.sweepCaches(caching)
+        Writeboost.Table(backing, caching).create { s =>
+          val st1 = s.status
+          s.bdev.write(Sector(0), DataBuffer.random(Sector(1).toB.toInt))
+          val st2 = s.status
+          val key1 = Writeboost.StatKey(true, false, false, false)
+          assert(st2.stat(key1) > st1.stat(key1))
+
+          s.bdev.read(Sector(8), Sector(1))
+          val st3 = s.status
+          val key2 = Writeboost.StatKey(false, false, false, false)
+          assert(st3.stat(key2) > st2.stat(key2))
+        }
+      }
+    }
+  }
   test("read: no caching") {
     slowDevice(Sector.G(1)) { backing =>
       fastDevice(Sector.M(32)) { caching =>
