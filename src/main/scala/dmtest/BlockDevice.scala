59432152b06deaa2e457482d63195d72645b2e9d
Akira Hayakawa <ruby.wktk@gmail.com>
2016-07-03 11:26:02 +0900

    tmp: tmppath in read/write
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/main/scala/dmtest/BlockDevice.scala
+++ src/main/scala/dmtest/BlockDevice.scala
@@ -1,37 +1,37 @@
 package dmtest
 
 import java.nio.ByteBuffer
-import java.nio.file.{StandardOpenOption, Files, Path}
+import java.nio.file.{Paths, StandardOpenOption, Files, Path}
 import scala.sys.process._
 
 case class BlockDevice(path: Path) {
   def size: Sector = Sector(Shell(s"blockdev --getsize ${path}").toLong)
   def zeroFill(): Unit = Shell(s"dd status=none if=/dev/zero of=${path} bs=512 count=${size}")
   def read(offset: Sector, len: Sector): DataBuffer = {
-    stack.Memory(len) { s =>
-      Shell(s"dd status=none bs=512 if=${path} iflag=direct skip=${offset.unwrap} of=${s.bdev.path} count=${len.unwrap}")
+      val tmpPath = Paths.get("/tmp/dmtest-tmp")
+      Shell(s"dd status=none bs=512 if=${path} iflag=direct skip=${offset.unwrap} of=${tmpPath} count=${len.unwrap}")
       val buf = Array.ofDim[Byte](len.toB.toInt)
-      val chan = Files.newByteChannel(s.bdev.path, StandardOpenOption.READ)
+      val chan = Files.newByteChannel(tmpPath, StandardOpenOption.READ)
       try {
         chan.position(0)
         chan.read(ByteBuffer.wrap(buf))
       } finally {
         chan.close()
+        Files.deleteIfExists(tmpPath)
       }
       DataBuffer(buf)
     }
-  }
   def write(offset: Sector, buf: DataBuffer): Unit = {
     val len = Sector(buf.size / 512)
-    stack.Memory(len) { s =>
-      val chan = Files.newByteChannel(s.bdev.path, StandardOpenOption.WRITE)
+    val tmpPath = Paths.get("/tmp/dmtest-tmp")
+      val chan = Files.newByteChannel(tmpPath, StandardOpenOption.WRITE)
       try {
         chan.position(0)
         assert(chan.write(buf.refByteBuffer) == buf.size)
       } finally {
         chan.close()
+        Files.deleteIfExists(tmpPath)
       }
-      Shell(s"dd status=none bs=512 if=${s.bdev.path} of=${path} oflag=direct seek=${offset.unwrap} count=${len.unwrap}")
+      Shell(s"dd status=none bs=512 if=${tmpPath} of=${path} oflag=direct seek=${offset.unwrap} count=${len.unwrap}")
     }
-  }
 }
