8428961321e40cb29d73aca0cba23a60fbeeb620
Akira Hayakawa <ruby.wktk@gmail.com>
2016-07-03 17:26:42 +0900

    add comment
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/main/scala/dmtest/BlockDevice.scala
+++ src/main/scala/dmtest/BlockDevice.scala
@@ -4,6 +4,10 @@ import java.nio.ByteBuffer
 import java.nio.file.{StandardOpenOption, Files, Path}
 import scala.sys.process._
 
+// We use in-memory file as buffer and use dd command to submit IOs as we expect.
+// SeekableByteChannel doesn't submit the device a partial IO because it does read-modify-write operation to submit small IOs.
+// To test the DM device correctly the behavior isn't appropriate.
+
 case class BlockDevice(path: Path) {
   def size: Sector = Sector(Shell(s"blockdev --getsize ${path}").toLong)
   def zeroFill(): Unit = Shell(s"dd status=none if=/dev/zero of=${path} bs=512 count=${size}")
