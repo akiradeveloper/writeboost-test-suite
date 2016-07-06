5abe28602281c91a2cd2cda7983dfcfbc2b91134
Akira Hayakawa <ruby.wktk@gmail.com>
2016-07-06 21:52:43 +0900

    wip
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/test/scala/dmtest/FlakeyTest.scala
+++ src/test/scala/dmtest/FlakeyTest.scala
@@ -1,10 +1,10 @@
 package dmtest
 
-import dmtest.stack.Flakey
+import dmtest.stack._
 
 class FlakeyTest extends DMTestSuite {
   test("flakey (read)") {
-    slowDevice(Sector.M(16)) { a =>
+    Memory(Sector.M(16)) { a =>
       Flakey.Table(a, 0, 1).create { b =>
         intercept[Exception] {
           Shell(s"dd status=none if=${b.bdev.path} iflag=direct of=/dev/null")
