8f944f6dbd1465a750e50cb37b0d39746830ceb5
Akira Hayakawa <ruby.wktk@gmail.com>
2016-07-17 19:59:14 +0900

    wip
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/test/scala/dmtest/writeboost/CompileRuby.scala
+++ src/test/scala/dmtest/writeboost/CompileRuby.scala
@@ -5,22 +5,21 @@ import java.nio.file.{Files, Paths, Path}
 import dmtest.Shell
 
 class CompileRuby(dir: Path) {
-  val RUBY = dir.resolve(s"ruby.tar.gz")
+  val WD = dir.resolve("ruby-2.1.1")
   val RUBY_CACHED = Paths.get("ruby-2.1.1.tar.gz")
   def downloadArchive: Unit = {
     if (!Files.exists(RUBY_CACHED)) {
       Shell(s"curl http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.1.tar.gz -o ${RUBY_CACHED}")
     }
-    Files.copy(RUBY_CACHED, RUBY)
   }
   def unarchive: Unit = {
-    Shell.at(dir)(s"tar xvfz ${RUBY}")
+    Shell.at(dir)(s"tar xvfz ${RUBY_CACHED}")
   }
   def compile: Unit = {
-    Shell.at(dir.resolve("ruby"))("./configure")
-    Shell.at(dir.resolve("ruby"))("make")
+    Shell.at(WD)("./configure")
+    Shell.at(WD)("make")
   }
   def check: Unit = {
-    Shell.at(dir.resolve("ruby"))("make test")
+    Shell.at(WD)("make test")
   }
 }
