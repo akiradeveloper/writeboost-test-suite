946e645d27281207ffabfc152a52259739e8c6d8
Akira Hayakawa <ruby.wktk@gmail.com>
2016-07-17 21:49:38 +0900

    wip
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/test/scala/dmtest/writeboost/WIP.scala
+++ src/test/scala/dmtest/writeboost/WIP.scala
@@ -6,6 +6,29 @@ import dmtest.fs._
 import scala.util.Random
 
 class WIP extends DMTestSuite {
-  // perf
-
+  test("dbench") {
+    slowDevice(Sector.G(2)) { backing =>
+      fastDevice(Sector.M(64)) { caching =>
+        Writeboost.sweepCaches(caching)
+        val table = Writeboost.Table(backing, caching,
+          Map("writeback_threshold" -> 70, "read_cache_threshold" -> 31)
+        )
+        def run(option: String): Unit = reportTime(option) {
+          table.create { s =>
+            XFS.format(s)
+            XFS.Mount(s) { mp =>
+              Shell.at(mp)(s"dbench ${option}")
+              Shell("sync")
+              Kernel.dropCaches
+              s.dropCaches()
+            }
+          }
+        }
+        val t = 300 <> 1
+        run(s"-t ${t} 4")
+        run(s"-S -t ${t} 4") // directory operations are sync
+        run(s"-s -t ${t} 4") // all operations are sync
+      }
+    }
+  }
 }
