4fe33d0c7883c283f9c36952d6bb483c7e2c35fd
Akira Hayakawa <ruby.wktk@gmail.com>
2016-07-17 20:18:27 +0900

    wip
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/test/scala/dmtest/writeboost/CompileRuby.scala
+++ src/test/scala/dmtest/writeboost/CompileRuby.scala
@@ -11,6 +11,7 @@ class CompileRuby(dir: Path) {
     if (!Files.exists(RUBY_CACHED)) {
       Shell(s"curl http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.1.tar.gz -o ${RUBY_CACHED}")
     }
+    Files.copy(RUBY_CACHED, dir.resolve(RUBY_CACHED))
   }
   def unarchive: Unit = {
     Shell.at(dir)(s"tar xvfz ${RUBY_CACHED}")
