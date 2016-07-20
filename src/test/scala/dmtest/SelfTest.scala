9ea470c732eff88b62572221a0611004962df574
Akira Hayakawa <ruby.wktk@gmail.com>
2016-07-20 16:31:05 +0900

    fix SelfTest
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/test/scala/dmtest/SelfTest.scala
+++ src/test/scala/dmtest/SelfTest.scala
@@ -4,12 +4,16 @@ import org.scalatest.Suites
 
 class SelfTest extends Suites(
   new MiscTest,
+  new DMStateTest,
   new DataBufferTest,
   new ConfigTest,
   new StackTest,
   new PoolTest,
+  new LuksTest,
+  new FlakeyTest,
   new DMStateTest,
   new FileSystemTest,
   new RandomPatternVerifierTest,
-  new PatternedSeqIOTest
+  new PatternedSeqIOTest,
+  new CreateRandomFilesTest
 )
