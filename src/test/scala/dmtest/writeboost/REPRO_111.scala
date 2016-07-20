85edd6713404f478bcf2b34923bab726f94b2143
Akira Hayakawa <ruby.wktk@gmail.com>
2016-07-20 12:10:10 +0900

    wip
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/test/scala/dmtest/writeboost/REPRO_111.scala
+++ src/test/scala/dmtest/writeboost/REPRO_111.scala
@@ -6,7 +6,7 @@ import dmtest.stack._
 
 class REPRO_111 extends DMTestSuite {
   // not reproduced yet
-  test("luks on top of writeboost") {
+  ignore("luks on top of writeboost") {
     slowDevice(Sector.G(10)) { backing =>
       fastDevice(Sector.G(1)) { caching =>
         Writeboost.sweepCaches(caching)
@@ -27,7 +27,7 @@ class REPRO_111 extends DMTestSuite {
       }
     }
   }
-  test("pattern verifier") {
+  ignore("pattern verifier") {
     slowDevice(Sector.M(128)) { backing =>
       fastDevice(Sector.M(16)) { caching =>
         Writeboost.sweepCaches(caching)
@@ -40,6 +40,26 @@ class REPRO_111 extends DMTestSuite {
             ps.stamp(5)
             assert(ps.verify())
             assert(ps.verify())
+          }
+        }
+      }
+    }
+  }
+  test("encrypted backing") {
+    slowDevice(Sector.M(128)) { backing =>
+      Luks(backing) { luks =>
+        EXT4.format(luks)
+        EXT4.Mount(luks) { mp =>
+          Shell(s"if=/dev/urandom of=${mp.resolve("f")} bs=1m count=80")
+        }
+        fastDevice(Sector.M(16)) { caching =>
+          Writeboost.sweepCaches(caching)
+          val options = Map(
+            "read_cache_threshold" -> 1
+          )
+          Writeboost.Table(backing, caching, options).create { s =>
+            Shell(s"fsck.ext4 -fn ${s.bdev.path}")
+            Shell(s"fsck.ext4 -fn ${s.bdev.path}")
           }
         }
       }
