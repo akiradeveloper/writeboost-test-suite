4b1778f83f4812872a14bb7df44b83eb01edeadf
Akira Hayakawa <ruby.wktk@gmail.com>
2016-08-03 21:47:09 +0900

    wip
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/test/scala/dmtest/writeboost/WriteAroundCachingTest.scala
+++ src/test/scala/dmtest/writeboost/WriteAroundCachingTest.scala
@@ -111,5 +111,37 @@ class WriteAroundCachingTest extends DMTestSuite {
       }
     }
   }
+  test("cancel cells") {
+    slowDevice(Sector.G(1)) { backing =>
+      fastDevice(Sector.M(64)) { caching =>
+        Writeboost.sweepCaches(caching)
+        Writeboost.Table(backing, caching, Map("read_cache_threshold" -> 1, "write_around_mode" -> 1)).create { s =>
+          import PatternedSeqIO._
+
+          val maxIOAmount = Sector.K(4) * 2047 * 2
+
+          val reader = new PatternedSeqIO(Seq(Read(Sector.K(4)), Skip(Sector.K(4))))
+          reader.maxIOAmount = maxIOAmount
+
+          val writer = new PatternedSeqIO(Seq(Write(Sector.K(4)), Skip(Sector.K(4))))
+          writer.maxIOAmount = maxIOAmount
+
+          reader.run(s) // stage all read date into the cells
+
+          writer.run(s) // cancel all cells (try remove this line)
+
+          reader.startingOffset = maxIOAmount
+          reader.run(s)
+          Thread.sleep(5000) // wait for injection
+
+          reader.startingOffset = Sector(0)
+          reader.run(s) // no read hits
+
+          val st = s.status.stat
+          assert(st(Writeboost.StatKey(false, true, false, true)) === 0)
+        }
+      }
+    }
+  }
 }
 
