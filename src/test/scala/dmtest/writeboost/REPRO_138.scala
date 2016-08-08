1009a162e1f30df601d76628cfb40cc93cee846b
Akira Hayakawa <ruby.wktk@gmail.com>
2016-08-08 23:23:07 +0900

    wip
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

---
+++ src/test/scala/dmtest/writeboost/REPRO_138.scala
@@ -0,0 +1,43 @@
+package dmtest.writeboost
+
+import dmtest._
+import dmtest.stack._
+
+class REPRO_138 extends DMTestSuite {
+  test("log rotates") {
+    slowDevice(Sector.G(1)) { backing =>
+      fastDevice(Sector.M(2)) { caching => // superblock + 2 segments
+        Writeboost.sweepCaches(caching)
+        Writeboost.Table(backing, caching, Map("nr_read_cache_cells" -> 127, "read_cache_threshold" -> 1, "write_around_mode" -> 1)).create { s =>
+          import PatternedSeqIO._
+
+          val U = Sector.K(4) * 127 * 2
+
+          val reader = new PatternedSeqIO(Seq(Read(Sector.K(4)), Skip(Sector.K(4))))
+
+          reader.startingOffset = U * 0
+          reader.maxIOAmount = U * 3
+          reader.run(s)
+          Thread.sleep(10000) // wait for injection
+          s.dropTransient()
+
+          // caches = [U2, U1]
+
+          reader.startingOffset = U * 0
+          reader.maxIOAmount = U * 1
+          reader.run(s)
+          assert(s.status.stat(Writeboost.StatKey(false, true, false, true)) === 0)
+          Thread.sleep(10000) // wait for injection
+          s.dropTransient()
+
+          // caches = [U2, U0]
+
+          reader.startingOffset = U * 2
+          reader.maxIOAmount = U * 1
+          reader.run(s)
+          assert(s.status.stat(Writeboost.StatKey(false, true, false, true)) === 127)
+        }
+      }
+    }
+  }
+}
