f880f9d381d0085d659fd301aa32e657f02615f2
Akira Hayakawa <ruby.wktk@gmail.com>
2016-08-08 23:53:29 +0900

    wip
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/test/scala/dmtest/writeboost/REPRO_138.scala
+++ src/test/scala/dmtest/writeboost/REPRO_138.scala
@@ -18,7 +18,7 @@ class REPRO_138 extends DMTestSuite {
           writer.startingOffset = U * 0
           writer.maxIOAmount = U * 3
           writer.run(s)
-          Thread.sleep(10000) // wait for flush
+          s.dropTransient()
 
           // caches = [U2, U1]
 
@@ -47,11 +47,23 @@ class REPRO_138 extends DMTestSuite {
           val U = Sector.K(4) * 127 * 2
 
           val reader = new PatternedSeqIO(Seq(Read(Sector.K(4)), Skip(Sector.K(4))))
-
           reader.startingOffset = U * 0
-          reader.maxIOAmount = U * 3
+          reader.maxIOAmount = U * 1
           reader.run(s)
           Thread.sleep(10000) // wait for injection
+          s.dropTransient()
+
+          reader.startingOffset = U * 1
+          reader.maxIOAmount = U * 1
+          reader.run(s)
+          Thread.sleep(10000) // wait for injection
+          s.dropTransient()
+
+          reader.startingOffset = U * 2
+          reader.maxIOAmount = U * 1
+          reader.run(s)
+          Thread.sleep(10000) // wait for injection
+          s.dropTransient()
 
           // caches = [U2, U1]
 
@@ -60,6 +72,7 @@ class REPRO_138 extends DMTestSuite {
           reader.run(s)
           assert(s.status.stat(Writeboost.StatKey(false, true, false, true)) === 0)
           Thread.sleep(10000) // wait for injection
+          s.dropTransient()
 
           // caches = [U2, U0]
 
