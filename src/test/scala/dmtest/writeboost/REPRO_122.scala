d225ac7d2d1be727b729e6b415d176eb8eb4ab9c
Akira Hayakawa <ruby.wktk@gmail.com>
2016-08-08 21:58:50 +0900

    wip
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/test/scala/dmtest/writeboost/REPRO_122.scala
+++ src/test/scala/dmtest/writeboost/REPRO_122.scala
@@ -8,11 +8,12 @@ class REPRO_122 extends DMTestSuite {
   test("nr_read_cache_cells works") {
     slowDevice(Sector.G(1)) { backing =>
       fastDevice(Sector.M(32)) { caching =>
-        Writeboost.sweepCaches(caching)
-        val m = Map("nr_read_cache_cells" -> 32, "read_cache_threshold" -> 1)
+        val m = Map("nr_read_cache_cells" -> 32, "read_cache_threshold" -> 1, "write_around_mode" -> 1)
         import PatternedSeqIO._
         val pat = Seq(Read(Sector.K(4)), Skip(Sector.K(4)))
         val pio = new PatternedSeqIO(pat)
+
+        Writeboost.sweepCaches(caching)
 
         Writeboost.Table(backing, caching, m).create { s =>
           pio.maxIOAmount = Sector.K(4) * 31 * 2
@@ -28,23 +29,23 @@ class REPRO_122 extends DMTestSuite {
         Writeboost.Table(backing, caching, m).create { s =>
           pio.maxIOAmount = Sector.K(4) * 32 * 2
           pio.run(s)
+          assert(s.status.stat(Writeboost.StatKey(false, true, false, true)) === 0)
           Thread.sleep(5000) // wait for injection
-
           s.dropTransient()
+
           pio.run(s)
-          val st = s.status.stat
-          assert(st(Writeboost.StatKey(false, true, false, true)) === 32)
+          assert(s.status.stat(Writeboost.StatKey(false, true, false, true)) === 32)
         }
 
         Writeboost.Table(backing, caching, m).create { s =>
           pio.maxIOAmount = Sector.K(4) * 33 * 2
           pio.run(s)
+          assert(s.status.stat(Writeboost.StatKey(false, true, false, true)) === 0)
           Thread.sleep(5000) // wait for injection
-
           s.dropTransient()
+
           pio.run(s)
-          val st = s.status.stat
-          assert(st(Writeboost.StatKey(false, true, false, true)) === 32)
+          assert(s.status.stat(Writeboost.StatKey(false, true, false, true)) === 32)
         }
       }
     }
