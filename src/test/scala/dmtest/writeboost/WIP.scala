6de93fef3c27773180edb283ba51d79911afeec2
Akira Hayakawa <ruby.wktk@gmail.com>
2016-08-08 21:35:50 +0900

    wip
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/test/scala/dmtest/writeboost/WIP.scala
+++ src/test/scala/dmtest/writeboost/WIP.scala
@@ -6,12 +6,11 @@ import dmtest.fs._
 import scala.util.Random
 
 class WIP extends DMTestSuite {
-  // FIXME nr_cache_cells
-  ignore("log rotates") {
+  test("log rotates") {
     slowDevice(Sector.G(1)) { backing =>
       fastDevice(Sector.M(2)) { caching => // superblock + 2 segments
         Writeboost.sweepCaches(caching)
-        Writeboost.Table(backing, caching, Map("nr_cache_cells" -> 127, "read_cache_threshold" -> 1, "write_around_mode" -> 1)).create { s =>
+        Writeboost.Table(backing, caching, Map("nr_read_cache_cells" -> 127, "read_cache_threshold" -> 1, "write_around_mode" -> 1)).create { s =>
           import PatternedSeqIO._
 
           val U = Sector.K(4) * 127 * 2
@@ -21,6 +20,7 @@ class WIP extends DMTestSuite {
           reader.maxIOAmount = U * 3
           reader.run(s) // stage all read date into the cells
           Thread.sleep(10000) // wait for injection
+          s.dropTransient()
 
           // caches = [U1, U2]
 
@@ -29,6 +29,7 @@ class WIP extends DMTestSuite {
           reader.run(s)
           assert(s.status.stat(Writeboost.StatKey(false, true, false, true)) === 0)
           Thread.sleep(10000) // wait for injection
+          s.dropTransient()
 
           // caches = [U2, U0]
 
