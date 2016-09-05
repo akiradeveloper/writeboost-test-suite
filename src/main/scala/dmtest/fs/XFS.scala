bcf43a19a75b19e71509257e3fd57ce340da59e5
Akira Hayakawa <ruby.wktk@gmail.com>
2016-09-05 22:13:30 +0900

    wip
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/main/scala/dmtest/fs/XFS.scala
+++ src/main/scala/dmtest/fs/XFS.scala
@@ -3,7 +3,7 @@ package dmtest.fs
 import dmtest.{Shell, Stack}
 
 object XFS {
-  def format(stack: Stack) = Shell(s"mkfs.xfs -f ${stack.bdev.path}")
+  def format(stack: Stack) = Shell(s"mkfs.xfs -f -m crc=1 ${stack.bdev.path}")
   case class Mount(stack: Stack) extends MountApply {
     override def mountOption: String = ""
     override def checkCmd: String = "xfs_repair -n"
