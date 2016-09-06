8b87fc4f546da2b07f90ff3d4c421d3e59e72261
Akira Hayakawa <ruby.wktk@gmail.com>
2016-09-07 00:34:42 +0900

    wip
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/test/scala/dmtest/WIP.scala
+++ src/test/scala/dmtest/WIP.scala
@@ -8,8 +8,19 @@ class WIP extends DMTestSuite {
     slowDevice(Sector.G(1)) { backing =>
       fastDevice(Sector.M(10)) { caching =>
         Writeboost.sweepCaches(caching)
-        Writeboost.Table(backing, caching, Map("write_around_mode" -> 1, "read_cache_threshold" -> 127)).create { s =>
+        Writeboost.Table(backing, caching, Map("write_around_mode" -> 1, "read_cache_threshold" -> 1)).create { s =>
+          import PatternedSeqIO._
 
+          val reader = new PatternedSeqIO(Seq(Read(Sector.K(4)), Skip(Sector.K(4))))
+          reader.maxIOAmount = Sector.G(1)
+          reader.run(s)
+
+          Thread.sleep(5000)
+
+          reader.maxIOAmount = Sector.G(1)
+          reader.run(s)
+
+          assert(s.status.currentId > s.status.nrSegments)
         }
       }
     }
