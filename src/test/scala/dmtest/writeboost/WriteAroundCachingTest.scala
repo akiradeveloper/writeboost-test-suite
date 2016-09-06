93a1ebb1d065c4f46baa5b53601e30a762cef5f3
Akira Hayakawa <ruby.wktk@gmail.com>
2016-09-07 00:48:28 +0900

    wip
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/test/scala/dmtest/writeboost/WriteAroundCachingTest.scala
+++ src/test/scala/dmtest/writeboost/WriteAroundCachingTest.scala
@@ -193,5 +193,28 @@ class WriteAroundCachingTest extends DMTestSuite {
       }
     }
   }
+  test("write-around can log rotate") {
+    slowDevice(Sector.G(1)) { backing =>
+      fastDevice(Sector.M(10)) { caching =>
+        Writeboost.sweepCaches(caching)
+        Writeboost.Table(backing, caching, Map("write_around_mode" -> 1, "read_cache_threshold" -> 1)).create { s =>
+          import PatternedSeqIO._
+
+          val reader = new PatternedSeqIO(Seq(Read(Sector.K(4)), Skip(Sector.K(4))))
+          reader.maxIOAmount = Sector.M(30)
+
+          reader.startingOffset = Sector(0)
+          reader.run(s)
+
+          Thread.sleep(5000)
+
+          reader.startingOffset = Sector.M(30)
+          reader.run(s)
+
+          assert(s.status.currentId > s.status.nrSegments)
+        }
+      }
+    }
+  }
 }
 
