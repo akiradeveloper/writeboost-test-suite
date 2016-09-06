2bf94649a49501cdbde62fd5f60cf42e1f8336a3
Akira Hayakawa <ruby.wktk@gmail.com>
2016-09-06 11:00:50 +0900

    wip
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/test/scala/dmtest/writeboost/REPRO_150.scala
+++ src/test/scala/dmtest/writeboost/REPRO_150.scala
@@ -70,11 +70,19 @@ class REPRO_150 extends DMTestSuite {
         val o2 = TempFile.alloc()
         val o3 = TempFile.alloc()
         Writeboost.Table(backing, caching, Map("write_around_mode" -> 1, "read_cache_threshold" -> 127)).create { s =>
+          val key = Writeboost.StatKey(false, true, false, true)
+          val s1 = s.status.stat(key)
           Shell(s"dd if=${s.bdev.path} of=${o1} bs=1M")
+          val s2 = s.status.stat(key)
+          assert(s2 === s1)
           Kernel.dropCaches
           Shell(s"dd if=${s.bdev.path} of=${o2} bs=1M")
+          val s3 = s.status.stat(key)
+          assert(s3 > s2)
           Kernel.dropCaches
           Shell(s"dd if=${s.bdev.path} of=${o3} bs=1M")
+          val s4 = s.status.stat(key)
+          assert(s4 > s3)
         }
 
         def sha1sum(f: Path) = {
@@ -84,6 +92,7 @@ class REPRO_150 extends DMTestSuite {
         }
 
         val h_baseline = sha1sum(baseline)
+        logger.debug(s"h_baseline = ${h_baseline}")
         assert(sha1sum(o1) === h_baseline)
         assert(sha1sum(o2) === h_baseline)
         assert(sha1sum(o3) === h_baseline)
