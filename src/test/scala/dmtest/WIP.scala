db32c0440a4cc51b3cdfbfdf9b89833196f1af5e
Akira Hayakawa <ruby.wktk@gmail.com>
2016-09-07 00:37:30 +0900

    wip
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/test/scala/dmtest/WIP.scala
+++ src/test/scala/dmtest/WIP.scala
@@ -4,26 +4,6 @@ import stack._
 import fs._
 
 class WIP extends DMTestSuite {
-  test("write-around can log rotate") {
-    slowDevice(Sector.G(1)) { backing =>
-      fastDevice(Sector.M(10)) { caching =>
-        Writeboost.sweepCaches(caching)
-        Writeboost.Table(backing, caching, Map("write_around_mode" -> 1, "read_cache_threshold" -> 1)).create { s =>
-          import PatternedSeqIO._
 
-          val reader = new PatternedSeqIO(Seq(Read(Sector.K(4)), Skip(Sector.K(4))))
-          reader.maxIOAmount = Sector.G(1)
-          reader.run(s)
-
-          Thread.sleep(5000)
-
-          reader.maxIOAmount = Sector.G(1)
-          reader.run(s)
-
-          assert(s.status.currentId > s.status.nrSegments)
-        }
-      }
-    }
-  }
 }
 
--- src/test/scala/dmtest/writeboost/WIP.scala
+++ src/test/scala/dmtest/writeboost/WIP.scala
@@ -6,5 +6,25 @@ import dmtest.fs._
 import scala.util.Random
 
 class WIP extends DMTestSuite {
+  test("write-around can log rotate") {
+    slowDevice(Sector.G(1)) { backing =>
+      fastDevice(Sector.M(10)) { caching =>
+        Writeboost.sweepCaches(caching)
+        Writeboost.Table(backing, caching, Map("write_around_mode" -> 1, "read_cache_threshold" -> 1)).create { s =>
+          import PatternedSeqIO._
 
+          val reader = new PatternedSeqIO(Seq(Read(Sector.K(4)), Skip(Sector.K(4))))
+          reader.maxIOAmount = Sector.M(30)
+          reader.run(s)
+
+          Thread.sleep(5000)
+
+          reader.maxIOAmount = Sector.M(30)
+          reader.run(s)
+
+          assert(s.status.currentId > s.status.nrSegments)
+        }
+      }
+    }
+  }
 }
