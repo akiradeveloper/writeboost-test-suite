79794e8ed8be1b3a2ebad6456898e888d92b4ca1
Akira Hayakawa <ruby.wktk@gmail.com>
2016-09-06 10:30:33 +0900

    wip
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/test/scala/dmtest/writeboost/REPRO_150.scala
+++ src/test/scala/dmtest/writeboost/REPRO_150.scala
@@ -7,7 +7,8 @@ import dmtest._
 import dmtest.stack._
 
 class REPRO_150 extends DMTestSuite {
-  test("read-caching works") {
+  // not reproduced
+  ignore("with filesystem") {
     slowDevice(Sector.G(10)) { backing =>
       fastDevice(Sector.G(1)) { caching =>
         fs.XFS.format(backing)
@@ -49,6 +50,40 @@ class REPRO_150 extends DMTestSuite {
             s.status
           }
         }
+      }
+    }
+  }
+
+  test("with device file") {
+    slowDevice(Sector.M(100)) { backing =>
+      fastDevice(Sector.M(10)) { caching =>
+        Shell.runScript { s"scrub -pcustom\"DATA\" -S ${backing}" }
+        Shell.runScript { s"scrub -pcustom\"CACHE\" -S ${caching}" }
+
+        val baseline = TempFile.alloc()
+        Shell.runScript { s"dd if=${backing.bdev.path} of=${baseline} bs=1M" }
+
+        Writeboost.sweepCaches(caching)
+        Shell("sync")
+
+        val o1 = TempFile.alloc()
+        val o2 = TempFile.alloc()
+        val o3 = TempFile.alloc()
+        Writeboost.Table(backing, caching, Map("write_around_mode" -> 1, "read_cache_threshold" -> 127)).create { s =>
+          Shell(s"dd if=${s.bdev.path} of=${o1} bs=1M")
+          Kernel.dropCaches
+          Shell(s"dd if=${s.bdev.path} of=${o2} bs=1M")
+          Kernel.dropCaches
+          Shell(s"dd if=${s.bdev.path} of=${o3} bs=1M")
+        }
+
+        val h_baseline = Shell.sync(s"sha1sum ${baseline}")
+        val h_o1 = Shell.sync(s"sha1sum ${o1}")
+        assert(h_o1 === h_baseline)
+        val h_o2 = Shell.sync(s"sha1sum ${o2}")
+        assert(h_o2 === h_baseline)
+        val h_o3 = Shell.sync(s"sha1sum ${o3}")
+        assert(h_o3 === h_baseline)
       }
     }
   }
